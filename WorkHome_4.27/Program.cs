c44105da7066d089c375220cac03c6f049a0bbc5
Maksim <bagzmax75@gmail.com>
2022-07-08 14:35:20 +0300

    add Work Home 4.27

---
+++ WorkHome_4.27/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+int GetSum(int N)
+{
+    int sum = 0;
+        for (; N!= 0; N /= 10)
+    {
+        sum += N % 10;
+    }
+    return sum;
+}
+
+Console.Write("Ведите число: ");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма цифр в числе {num}: " + GetSum(num));
